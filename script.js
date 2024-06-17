ef93f839ac297bc3a9a35a6e1db77f0777046737
Darshana Durgamahanty <darshanadurgamahanty@gmail.com>
2024-06-18 00:25:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+// const sname = document.getElementById("name")
+
+// title.innerText = "To my lovely, handsome Kaashu Paaji💖";
+
+desc.innerText = "ik i mean a lot to you 😇 I'm always here to irritate you 🥰 just wish that God gives you more power to handle it!! \n Appie b'day my kashu paaji, secret keeper in all the crazy adventures i do 🫣 \n I wish you all the happiness, success✨ nd lots of love you deserve 💕 \n Thank me for being the best sister anyone could ask for 😇";
+
+// sname.innerText = "Paajii❤️";
